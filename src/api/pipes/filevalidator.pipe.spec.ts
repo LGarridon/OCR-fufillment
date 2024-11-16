8a018f1f68f0b6f618cb050df1befcc16df21df9
Omar <omarmoper@gmail.com>
2024-11-17 00:45:14 +0100

    Endpoint to get text from images

---
+++ src/api/pipes/filevalidator.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { FileValidatorPipe } from './filevalidator.pipe';
+
+describe('ApiPipe', () => {
+  it('should be defined', () => {
+    expect(new FileValidatorPipe()).toBeDefined();
+  });
+});
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OcrModule } from './ocr/ocr.module';
+import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [OcrModule],
+  imports: [OcrModule, ApiModule],
   controllers: [AppController],
   providers: [AppService],
 })
